c287fd0efbb7a80c3f832d936bccb78c7fa3e260
404PeterSimoes <176055646+404PeterSimoes@users.noreply.github.com>
2026-05-13 14:38:47 +0100

    Section 15 - 242 - Add Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import AppRoutes from './routes/AppRouter';
+import Header from './components/layout/Header';
 
 function App() {
   return (
     <>
-      <div>Hello</div>
+      <Header />
       <AppRoutes />
     </>
   );
