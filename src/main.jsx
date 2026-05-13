9c1c87d1ba653d614dd27d91b5b880027c54a7c5
404PeterSimoes <176055646+404PeterSimoes@users.noreply.github.com>
2026-05-13 15:19:58 +0100

    Section 16- 249 - Add Redux Store

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,15 @@ import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { Provider } from 'react-redux';
+import store from './store/store.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 );
