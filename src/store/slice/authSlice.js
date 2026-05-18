96f7d335622ee8313fb74496fe38492571bd6935
404PeterSimoes <176055646+404PeterSimoes@users.noreply.github.com>
2026-05-18 15:50:50 +0100

    Section 19 - 293 & 294 - Initial State for Invalid and Valid Token

---
+++ src/store/slice/authSlice.js
@@ -0,0 +1,46 @@
+import { getUserInfoFromToken, isTokenExpired } from '../../utility/jwtUtility';
+import { createSlice } from '@reduxjs/toolkit';
+
+const STORAGE_KEYS = {
+  TOKEN: 'token-mango',
+  USER: 'user-mango',
+};
+
+const getInitialAuthState = () => {
+  const storedToken = localStorage.getItem(STORAGE_KEYS.TOKEN);
+  const storedUser = localStorage.getItem(STORAGE_KEYS.USER);
+
+  // Clear invalid token
+  if (
+    !token ||
+    token === undefined ||
+    token === null ||
+    isTokenExpired(token)
+  ) {
+    localStorage.removeItem(STORAGE_KEYS.TOKEN);
+    localStorage.removeItem(STORAGE_KEYS.USER);
+
+    return {
+      user: null,
+      token: null,
+      isAuthenticated: false,
+    };
+  }
+
+  let user = null;
+  if (storedUser && storedUser !== undefined && storedUser !== null) {
+    try {
+      user = JSON.parse(storedToken);
+    } catch {
+      user = getUserInfoFromToken(token);
+      if (user) {
+        localStorage.setItem(STORAGE_KEYS.USER, JSON.stringify(user));
+      }
+    }
+  }
+  return {
+    user,
+    token,
+    isAuthenticated: !!token && !!user,
+  };
+};
