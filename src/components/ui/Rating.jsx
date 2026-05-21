4778ec948db13de2cb4a702a4bd647a313fc97ea
404PeterSimoes <176055646+404PeterSimoes@users.noreply.github.com>
2026-05-21 10:03:53 +0100

    Section 23 - 337 - Login to calculate the rating

--- src/components/ui/Rating.jsx
+++ src/components/ui/Rating.jsx
@@ -6,7 +6,15 @@ function Rating(value = 0, size = 'medium', onChange = null) {
   const sizeClass =
     size === 'small' ? 'fs-6' : size === 'large' ? 'fs-4' : 'fs-5';
 
-  const renderStar = (index) => {};
+  const renderStar = (index) => {
+    const rating = isInteractive ? hover || value : value;
+    const isFilled = index <= rating;
+    const roundedRating = Math.round(rating * 2) / 2; // round to the nearest 0.5
+    const isHalf =
+      !isInteractive &&
+      roundedRating % 1 === 0.5 &&
+      index === Math.ceil(roundedRating);
+  };
 
   return <i class="bi bi-star-half"></i>;
 }
